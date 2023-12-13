11272ea90d1ac416662ba09611dbc6cae558354b
Mario Silva <mariosilva.81@icloud.com>
2023-12-13 07:46:09 -0300

    Initial commit

---
+++ src/repositories/categories.repository.js
@@ -0,0 +1,13 @@
+const dbConnection = require('../database/connection/index');
+
+const categories = {
+  findAll: ()=>{
+    return dbConnection.query(`SELECT * FROM categorias;`);
+  },
+
+  findByPk: (id)=>{
+    return dbConnection.query(`SELECT * FROM categorias WHERE id = $1;`, [id]);
+  }
+}
+
+module.exports = categories;
